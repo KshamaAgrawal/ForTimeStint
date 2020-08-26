3f03e6d58668ecb6a861325ec2bda221a531a0c3
KshamaAgrawal <kshama@securitytroops.com>
2020-08-26 12:28:17 +0530

    profile psge is ready

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,41 +5,16 @@ import 'screenshots.dart';
 import 'login_screen.dart';
 import 'dashboard.dart';
 import 'profile.dart';
-// import 'package:first_app/CustomPopupMenu.dart';
 
 class profilePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _State();
 }
-// List choices = [
-//   CustomPopupMenu(title: 'Home', icon: Icons.home),
-//   CustomPopupMenu(title: 'Bookmarks', icon: Icons.bookmark),
-//   CustomPopupMenu(title: 'Settings', icon: Icons.settings),
-// ];
 
 class _State extends State<profilePage>{
-//   CustomPopupMenu _selectedChoices = choices[0];
-
-//   List choices = [
-//   CustomPopupMenu(title: 'Home', icon: Icons.home),
-//   CustomPopupMenu(title: 'Bookmarks', icon: Icons.bookmark),
-//   CustomPopupMenu(title: 'Settings', icon: Icons.settings),
-// ];
-
-//   // CustomPopupMenu _selectedChoices = choices[0];
-
-//   void _select(CustomPopupMenu choice) {
-//     setState(() {
-//       _selectedChoices = choice;
-//     });
-//   }
-  // PopupMenu.context = context;
 
   @override  
   Widget build(BuildContext context){
-  	// return Container(
-   //    child: SelectedOption(choice: _selectedChoices),
-   //  );
     return Scaffold(
   	  appBar: AppBar(
         iconTheme: new IconThemeData(color: Colors.blue),
@@ -55,82 +30,8 @@ class _State extends State<profilePage>{
 		      ),
 		    ),
 		],
-     //    actions: [
-	    //     PopupMenuButton(
-	    //         elevation: 3.2,
-	    //         initialValue: choices[1],
-	    //         onCanceled: () {
-	    //           print('You have not chossed anything');
-	    //         },
-	    //         tooltip: 'This is tooltip',
-	    //         onSelected: _select,
-	    //         itemBuilder: (BuildContext context) {
-	    //           return choices.map((CustomPopupMenu choice) {
-	    //             return PopupMenuItem(
-	    //               value: choice,
-	    //               child: Text(choice.title),
-	    //             );
-	    //           }).toList();
-	    //         },
-	    //     ),
-	    // ],
-
-// 	    PopupMenu menu = PopupMenu(
-//       items: [
-//         MenuItem(title: 'Copy', image: Image.asset('assets/copy.png')), 
-//         MenuItem(title: 'Home', image: Icon(Icons.home, color: Colors.white,)), 
-//         MenuItem(title: 'Mail', image: Icon(Icons.mail, color: Colors.white,)), 
-//         MenuItem(title: 'Power', image: Icon(Icons.power, color: Colors.white,)),
-//         MenuItem(title: 'Setting', image: Icon(Icons.settings, color: Colors.white,)), 
-//         MenuItem(title: 'Traffic', image: Icon(Icons.traffic, color: Colors.white,))], 
-//       onClickMenu: onClickMenu, 
-//       stateChanged: stateChanged,
-//       onDismiss: onDismiss);
-
-// menu.show(rect: rect);
-
-// void stateChanged(bool isShow) {
-//   print('menu is ${ isShow ? 'showing': 'closed' }');
-// }
       ),
       body: Column(children: <Widget>[
-      	// Row(
-      	//   children: [
-      	//     Container(
-      	//       padding: EdgeInsets.fromLTRB(210,0,0,0),
-	      //     height: 90,
-	      //     child: Card(
-	      //     	elevation: 70,
-       //      	child: Column(children: <Widget>[
-       //            Row(
-       //              children: [
-       //                Container(
-       //                  padding: EdgeInsets.fromLTRB(0,10,40,0),
-       //                  child: Text('Edit', style: TextStyle(fontSize: 15),),
-       //                ), 
-       //              ]
-       //            ),
-       //            Row(
-       //              children: [
-       //                Container(
-       //                  padding: EdgeInsets.fromLTRB(0,0,20,0),
-       //                  child: FlatButton(
-		     //              child: Text('Logout', style: TextStyle(fontSize: 15),),
-		     //              onPressed: () {  
-		     //                Navigator.push(  
-		     //                  context,  
-		     //                  MaterialPageRoute(builder: (context) => LoginPage()),  
-		     //                );  
-		     //              }, 
-		     //            ),                        
-       //                ), 
-       //              ]
-       //            ),
-       //          ]),
-	      //     ),
-      	//     ),
-      	//   ],
-      	// ),
       	Row(
       	  children: [
       	    Container(
